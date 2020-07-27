769703e9de6f5e270d373f308f0ca33cfea44eef
Sebastien Gissinger <sgissinger@outlook.com>
2020-07-28 01:20:29 +0200

    simplify requires

--- src/fr-fr/fr-fr.11tydata.js
+++ src/fr-fr/fr-fr.11tydata.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const i18n = require('eleventy-plugin-i18n-gettext/i18n')
+const i18n = require('eleventy-plugin-i18n-gettext')
 
 module.exports = () => {
     return i18n.enhance11tydata({}, 'fr-fr')
