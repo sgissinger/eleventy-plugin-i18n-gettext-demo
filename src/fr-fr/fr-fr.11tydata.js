b8ba86da355bfd7be56f2fdc55015fbb20004526
Sebastien Gissinger <sgissinger@outlook.com>
2020-07-28 12:02:56 +0200

    add custom data to french

--- src/fr-fr/fr-fr.11tydata.js
+++ src/fr-fr/fr-fr.11tydata.js
@@ -3,5 +3,9 @@
 const i18n = require('eleventy-plugin-i18n-gettext')
 
 module.exports = () => {
-    return i18n.enhance11tydata({}, 'fr-fr')
+    const customData = {
+        eatSnails: true
+    }
+
+    return i18n.enhance11tydata(customData, 'fr-fr')
 }
